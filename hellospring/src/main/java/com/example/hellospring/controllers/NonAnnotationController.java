99642d599025bec13ace9ab1391077bb7b2ea263
오경환 <okyeonghwan5@gmail.com>
2021-04-13 16:40:37 +0900

    Non Annotation 방식 Bean 생성

---
+++ hellospring/src/main/java/com/example/hellospring/controllers/NonAnnotationController.java
@@ -0,0 +1,24 @@
+package com.example.hellospring.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+// Non Annotation 방식
+// -> handleRequest 메서드에 대응된다.
+public class NonAnnotationController implements Controller{
+
+//	Servlet 방식으로 request, response만 전달
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, 
+			HttpServletResponse response) throws Exception {
+	
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("message", "<h3>Non Annotation Controller</h3>");
+		mav.setViewName("/WEB-INF/views/hello.jsp");
+		return mav;
+	}
+
+}
